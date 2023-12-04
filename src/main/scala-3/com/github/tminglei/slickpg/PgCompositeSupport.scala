b41087ddf45cf04b1b5aecb1469e762e612c84ce
Hugh Simpson <hsimpson@rzsoftware.com>
2023-12-04 16:55:37 +0000

    i have no idea what im doing

--- src/main/scala-3/com/github/tminglei/slickpg/PgCompositeSupport.scala
+++ src/main/scala-3/com/github/tminglei/slickpg/PgCompositeSupport.scala
@@ -10,13 +10,33 @@ import slick.jdbc.SetParameter
 
 import scala.deriving.*
 import scala.compiletime.{error, erasedValue, summonInline}
+import utils.PgTokenHelper._
+import utils.TypeConverters._
+import utils.RegisteredTypeConverter
+
+case class Args(args: Any*)
+
+sealed trait TokenConverter[T] {
+  def fromToken(token: Token): T
+
+  def toToken(value: T): Token
+}
+
+
+//case class SimpleConverter(fromString: (String => T), ToString: (T => String), level: Int) extends TokenConverter[T] {
+//  def fromToken(token: Token): Any =
+//    if (token == Null) null else fromString(getString(token, level))
+//
+//  def toToken(value: Any): Token =
+//    if (value == null) Null else Chunk(ToString(value))
+//}
 
 trait PgCompositeSupport extends utils.PgCommonJdbcTypes with array.PgArrayJdbcTypes { driver: PostgresProfile =>
 
   protected lazy val emptyMembersAsNull = true
 
   //---
-  def createCompositeJdbcType[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]): GenericJdbcType[T] =
+  def createCompositeJdbcType[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader): GenericJdbcType[T] =
     throw new UnsupportedOperationException("Composite support is unimplemented for scala 3")
 
   def createCompositeArrayJdbcType[T <: Struct](sqlTypeName: String, cl: ClassLoader = getClass.getClassLoader)(implicit ev: TTag[T], tag: ClassTag[T]): AdvancedArrayJdbcType[T] =
@@ -41,6 +61,64 @@ trait PgCompositeSupport extends utils.PgCommonJdbcTypes with array.PgArrayJdbcT
 }
 
 class PgCompositeSupportUtils(cl: ClassLoader, emptyMembersAsNull: Boolean) {
+  import scala.deriving.*
+  import scala.compiletime.{error, erasedValue, summonInline}
+
+  implicit def baseConverter[T](using fromString: RegisteredTypeConverter[String, T], toString: RegisteredTypeConverter[T, String]): TokenConverter[T] = new TokenConverter[T] {
+    def fromToken(token: Token): T = if (token == Null) null.asInstanceOf[T] else fromString.convert(getString(token, level))
+
+    def toToken(value: T): Token = if (value == null) Null else Chunk(toString.convert(value))
+  }
+
+  inline def summonInstances[T, Elems <: Tuple]: List[TokenConverter[?]] = {
+    inline erasedValue[Elems] match {
+      case _: (elem *: elems) => deriveOrSummon[T, elem] :: summonInstances[T, elems]
+      case _: EmptyTuple => Nil
+    }
+  }
+
+  inline def deriveOrSummon[T, Elem]: TokenConverter[Elem] = {
+    inline erasedValue[Elem] match {
+      case _: T => deriveRec[T, Elem]
+      case _ => summonInline[TokenConverter[Elem]]
+    }
+  }
+
+  inline def deriveRec[T, Elem]: TokenConverter[Elem] = {
+    inline erasedValue[T] match {
+      case _: Elem => error("infinite recursive derivation")
+      case _ => derived[Elem](using summonInline[Mirror.Of[Elem]]) // recursive derivation
+    }
+  }
+
+  def convertProduct[T](p: Mirror.ProductOf[T], elems: => List[TokenConverter[?]]): TokenConverter[T] =
+    new TokenConverter[T] {
+      def fromToken(token: Token): T =
+        if (token == Null) null
+        else {
+          val args =
+            getChildren(token)
+              .zip(elems)
+              .map { case (token, converter) => converter.fromToken(token) }
+          p.fromProduct(Args(args:_*))
+        }
+
+      def toToken(value: T): Token =
+        if (value == null) Null
+        else {
+          val tokens = value.productIterator.zip(elems).map({
+            case (value, converter) => converter.toToken(value)
+          })
+          val members = Open("(") +: tokens :+ Close(")")
+          GroupToken(members)
+        }
+    }
+
+  inline def derived[T <: Struct](using m: Mirror.Of[T]): TokenConverter[T] = {
+    lazy val elemInstances = summonInstances[T, m.MirroredElemTypes]
+    inline m match
+      case p: Mirror.ProductOf[T] => convertProduct(p, elemInstances)
+  }
 
 //  def mkTokenConverter(theType: u.Type, level: Int = -1)(implicit ev: u.TypeTag[String]): TokenConverter = ???
 }
